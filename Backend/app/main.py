9caec5e01dd63aefa9c24e5eeecf7e34bf06ae75
Shumail Khan <shumailkhan7878@gmail.com>
2026-04-08 23:50:34 +0500

    Updated Video Streaming added.

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.responses import FileResponse
 
 app = FastAPI(title="WDUP Backend API")
 app.mount("/media", StaticFiles(directory="media"), name="media")
@@ -9,6 +9,5 @@ app.mount("/media", StaticFiles(directory="media"), name="media")
 app.include_router(router)
 
 @app.get("/")
-def root():
-    print("WDUP Backend is running.")
-    return {"message": "WDUP Backend Running"}
+def index():
+    return FileResponse("video.html")
